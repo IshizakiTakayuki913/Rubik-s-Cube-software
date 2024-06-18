a291e54009260931312a5f98b8053ffac356e4e9
takayuki <vhsusouqah2002.h14.0@gmail.com>
2024-06-19 07:54:22 +0900

    Cube状態判定機能の追加

--- colorinput.js
+++ colorinput.js
@@ -56,7 +56,7 @@ const colorinput = () => ({
     this.menu.addEventListener("click",(e) => {
       if(e.target.classList[0] != "menu-colors") return
       const id = parseInt(e.target.classList[1][1])
-      console.log(id)
+      // console.log(id)
       if(this.choiceColor != id){
         if(this.choiceColor !== undefined)
           this.menu.children[this.choiceColor].classList.remove("choice-color")
@@ -108,6 +108,17 @@ const colorinput = () => ({
       e: [275, 550, 1100, 2185, 240, 3840],
       c: [51, 102, 204, 153, 15, 240]
     }
+    const face_row = {
+      c:[
+        [4,3,0],[4,0,1],[4,1,2],[4,2,3],
+        [5,0,3],[5,1,0],[5,2,1],[5,3,2]
+      ],
+      e:[
+        [0,3],[0,1],[2,1],[2,3],
+        [4,0],[4,1],[4,2],[4,3],
+        [5,0],[5,1],[5,2],[5,3],
+      ],
+    }
 
     let D = this.cData
       
@@ -116,30 +127,217 @@ const colorinput = () => ({
       for(let n of pos)	a &= CTS[type][n]
       return ((Math.log2(a)<0)?-1:Math.log2(a))
     }
-    let check = []
-    for(let t of ["e","c"]){
-      // console.log(t)
-      for(let y of D[t]){
-        check.push(num(t,y))
+    function parts_check(D){
+      let c = []
+      for(let t of ["e","c"]){
+        // console.log(t)
+        for(let y of D[t]){
+          c.push(num(t,y))
+        }
+      }
+      return c
+    }
+    function oll_input(D){
+      const a = {c:[],e:[]}
+      for(i=0;i<8;i++){
+        for(j=0;j<3;j++){
+          if(D.c[i][j] == -1)  a.c.push([i,j])
+        }
+      }
+      for(i=0;i<12;i++){
+        for(j=0;j<2;j++){
+          if(D.e[i][j] == -1)  a.e.push([i,j])
+        }
+      }
+      return {
+        check: (a.c.length == 0 && a.e.length == 0),
+        count: a,
       }
     }
+    function color_count(D){
+      const a = []
+      let T =true
+      for(i=0;i<D.count.length;i++){
+        if(D.count[i] != 0){
+          a.push([i,D.count[i]])
+          T = false
+        }
+      }
+      return {check: T, count: a}
+    }
+    function compareNumbers(a, b) {
+      return b - a;
+    }
+    function face_row_check(D){
+      // console.log(D.c)
+      const row = face_row
+      const cp_hit = [-1,-1,-1,-1,-1,-1,-1,-1]
+      const ep_hit = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+      for(let i=0;i<8;i++){
+        const A = D.c[i]
+        // console.log(`A [${A.join(' ')}]`)
+        for(let s=0;s<row.c.length;s++){
+          let A_count = 0
+          let limit = 3
+          let hit = true
+          const B = row.c[s]
+          // console.log(`  B [${B.join(' ')}]`)
+          for(let f=0;f<limit;f++){
+            // console.log(`    A_count[${A_count}] f%3[${f%3}] A[${A[A_count]}] == B[${B[f%3]}]`)
+            if(A[A_count] == B[f%3]){
+              A_count += 1
+              if(hit){
+                hit=false
+                limit = f + 3
+              }  
+            }
+          }
+          if(A_count==3){
+            // console.log(`- true -`)
+            cp_hit[i] = s
+            break
+          }
+        }
+      }
+      
+      for(let i=0;i<12;i++){
+        const A = D.e[i]
+        // console.log(`A [${A.join(' ')}]`)
+        for(let s=0;s<row.e.length;s++){
+          let A_count = 0
+          let limit = 2
+          let hit = true
+          const B = row.e[s]
+          // console.log(`  B [${B.join(' ')}]`)
+          for(let f=0;f<limit;f++){
+            // console.log(`    A_count[${A_count}] f%3[${f%3}] A[${A[A_count]}] == B[${B[f%3]}]`)
+            if(A[A_count] == B[f%2]){
+              A_count += 1
+              if(hit){
+                hit=false
+                limit = f + 2
+              }  
+            }
+          }
+          if(A_count==2){
+            // console.log(`- true -`)
+            ep_hit[i] = s
+            break
+          }
+        }
+      }
 
-    let sum_c = 1
-    for(let i of check) sum_c *= (i+1)
-    // console.log(`sum_c [${sum_c>0}] [${check.join(',')}]`)
-    if(sum_c == 0) {return false}
-    
-		let cp = check.slice(12)
-		let co = []
-    for(let i of D.c)
-      co.push(i[0]>3?0:(i[1]>3?1:2))
+      const cp_sort = cp_hit.concat()
+      const ep_sort = ep_hit.concat()
+      
+      cp_sort.sort(compareNumbers)
+      ep_sort.sort(compareNumbers)
 
-		let ep = check.slice(0,12)
-		let eo = []
-    for(let i of D.e)
-      eo.push((i[0]>3)?0:((i[1]>3)?1:((i[0]%2==0)?0:1)))
+      const cp_count = [0,0,0,0,0,0,0,0]
+      const ep_count = [0,0,0,0,0,0,0,0,0,0,0,0]
 
-    let solved_state = new State(cp, co, ep, eo)
+      for(let i of cp_sort) i>=0?cp_count[i]++:true
+      for(let i of ep_sort) i>=0?ep_count[i]++:true
+
+      let cp_bit = 1,ep_bit = 1
+      for(let i of cp_count)  cp_bit&=i
+      for(let i of ep_count)  ep_bit&=i
+
+      // console.log(cp_hit)
+      // console.log(ep_hit)
+      // console.log(cp_count)
+      // console.log(ep_count)
+
+      // console.log(cp_bit&ep_bit==1?"face_row_check OK":"falsface_row_check NO")
+
+      return {
+        check: (cp_bit&ep_bit)==1,
+        cp_hit: cp_hit,
+        ep_hit: ep_hit,
+        cp_count: cp_count,
+        ep_count: ep_count,
+      }
+    }
+    function ver_check(D){
+      let co_vec = []
+      for(let i of D.c)
+        co_vec.push(i[0]>3?0:(i[1]>3?1:2))
+
+      let eo_vec = []
+      for(let i of D.e)
+        eo_vec.push((i[0]>3)?0:((i[1]>3)?1:((i[0]%2==0)?0:1)))
+
+      const co_v_count = co_vec.reduce((a, b) => a + b, 0,)
+      const eo_v_count = eo_vec.reduce((a, b) => a + b, 0,)
+
+      // console.log(`co_v [${co_v_count%3}] eo_v [${eo_v_count%2}]`)
+
+      return {
+        check: (co_v_count%3+eo_v_count%2)==0,
+        co: co_vec,
+        eo: eo_vec,
+        co_v: co_v_count%3,
+        eo_v: eo_v_count%2,
+      }
+    }
+    function pos_parity_check(C, P){
+      let cp =  C.concat(), ep = P.concat()
+      const check = {check:false, c:0, e:0}
+      for(let i=0;i<7;i++){
+        for(let s=i+1;s<8;s++){
+          if(cp[i]>cp[s]){
+            box = cp[i]
+            cp[i] = cp[i]
+            cp[i] = box
+            check.c += 1
+          }
+        }
+      }
+      
+      for(let i=0;i<11;i++){
+        for(let s=i+1;s<12;s++){
+          if(ep[i]>ep[s]){
+            box = ep[i]
+            ep[i] = ep[i]
+            ep[i] = box
+            check.e += 1
+          }
+        }
+      }
+      check.c %= 2
+      check.e %= 2
+      check.check = (check.c == check.e)
+      return check
+    }
+
+    // console.log(C1 == true?"color_count OK":C1)
+    // console.log(C2 == true?"oll_input OK":C2)
+    // console.log(check)
+    const C1 = color_count(D)     //入力されている色の数が正しいか
+    const C2 = oll_input(D)       //全ての面に色が入力されているか
+    const C3 = face_row_check(D)  //partsとしてあり得るか
+    const C4 = ver_check(D)       //向きがあっているか
+    const C5 = pos_parity_check(C3.cp_hit, C3.ep_hit)       //向きがあっているか
+
+    // console.log(C1)
+    // console.log(C2)
+    // console.log(C3)
+    // console.log(C4)
+    // console.log(C5)
+
+    this.check_list = {
+      c1: C1,
+      c2: C2,
+      c3: C3,
+      c4: C4,
+      c5: C5,
+    }
+
+    if(!(C1.check && C2.check && C3.check && C4.check && C5.check))  return false
+
+    console.log("------OK-----")
+    let solved_state = new State(C3.cp_hit, C4.co, C3.ep_hit, C4.eo)
+    console.log(D)
     console.log(solved_state)
     return solved_state
   },
@@ -165,9 +363,14 @@ const colorinput = () => ({
     else if(parts_type === "corner")
       num = pos.face.normal.y > 0.707 ? 0 : (pos.face.normal.x > 0.707 ? 1 : 2)
 
-    console.log(`Color_set type [${parts_type}] num [${be}]index [${num}]`)
+    // console.log(`Color_set type [${parts_type}] num [${be}]index [${num}]`)
     face = {parts: parts.object.el, type: parts_type, Pindex: parseInt(be), Findex: num}
-    console.log(face)
+    // console.log(face)
+
+    
+    if(this.cData[face.type[0]][face.Pindex][face.Findex]  == this.choiceColor) return
+    if(this.cData[face.type[0]][face.Pindex][face.Findex]  != -1)
+      this.cData.count[this.cData[face.type[0]][face.Pindex][face.Findex]] += 1
 
     
     const f = face.parts.object3D.children[0].children[0].children
@@ -175,15 +378,18 @@ const colorinput = () => ({
     f[face.Findex].material.color = set_color_data[this.choiceColor]
     // console.log(`type [${face.type[0]}] Pin [${[face.Pindex]}] Fin [${face.Findex}]`)
     // console.log(this.cData)
+
     this.cData[face.type[0]][face.Pindex][face.Findex] = this.choiceColor
     this.cData.count[this.choiceColor] -= 1
-    console.log(this.cData)
+    // console.log(this.cData)
     Check_ans = this.Check_pos(face.type[0],face.Pindex)
+    // console.log(Check_ans)
     
     if(Check_ans === false) return
 
     // this.data.cube_mode="stay"
     const mode  = document.getElementById("scene").components["cube-mode"]
+    
     mode.Color_completion(Check_ans)
     this.menu.style.display = "none"
   },
